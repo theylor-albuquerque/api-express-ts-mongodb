6b85fff18f95fd0a27c37bf527c69a9102411577
Theylor Albuquerque <theylor.oliveira@icc.org.br>
2026-04-22 15:05:16 -0300

    feat: add express initial server

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('hello world!');
+})
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
