6add094f6cd82f65dbf5a2335198b18b3cbdfd81
Theylor Albuquerque <theylor.oliveira@icc.org.br>
2026-04-22 15:53:52 -0300

    feat: created .prettier file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import * as dotenv from "dotenv";
+import express from 'express';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-    res.send('hello world!');
-})
+  res.send('hello world!');
+});
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
