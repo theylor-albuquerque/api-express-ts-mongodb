0238bac8dca0e5dcb45a6c150a5241b3af6bdc14
Theylor Albuquerque <theylor.oliveira@icc.org.br>
2026-04-27 17:54:10 -0300

    feat: add MongoDeleteRepository

---
+++ src/repositories/deleteUser/mongoDeleteUser.ts
@@ -0,0 +1,28 @@
+import { ObjectId } from 'mongodb';
+import { IDeleteUserRepository } from '../../controllers/deleteUser/protocols';
+import { MongoClient } from '../../database/mongo';
+import { User } from '../../models/user';
+
+export class MongoDeleteUserRepository implements IDeleteUserRepository {
+  async deleteUser(id: string): Promise<User> {
+    const user = await MongoClient.db
+      .collection<Omit<User, 'id'>>('users')
+      .findOne({ _id: new ObjectId() });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const { deletedCount } = await MongoClient.db
+      .collection('user')
+      .deleteOne({ _id: new ObjectId(id) });
+
+    if (!deletedCount) {
+      throw new Error('User not deleted');
+    }
+
+    const { _id, ...rest } = user;
+
+    return { id: _id.toHexString(), ...rest };
+  }
+}
